77dccfa27f8b8e781d004b9caaf0e7581eeca644
Diogo Martins <me.martinsfe@gmail.com>
2022-11-30 17:59:05 -0300

    fix: lint

--- src/helpers/RenderWithRouter.js
+++ src/helpers/RenderWithRouter.js
@@ -6,7 +6,7 @@ import { Router } from 'react-router-dom';
 const renderWithRouter = (component) => {
   const history = createMemoryHistory();
   return ({
-    ...render(<Router history={history}>{component}</Router>), history,
+    ...render(<Router history={ history }>{component}</Router>), history,
   });
 };
 
