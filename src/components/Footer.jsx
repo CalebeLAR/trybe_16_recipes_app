147683536c8ace6c5eb2f0d1c8937a6a1fcf89d5
Matheus <matheuspaiva93@gmail.com>
2022-11-30 15:04:15 -0300

    requsitos 16-18 completos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,29 +7,24 @@ export default function Footer() {
   const history = useHistory();
 
   return (
-    <>
-      <div>
-        <img src={ drink } alt="drink" />
-        <button
-          type="button"
-          onClick={ () => history.push('/drinks') }
-          data-testid="drinks-bottom-btn"
-        >
-          página de bebidas
-        </button>
-      </div>
-      <div>
-        <img src={ food } alt="drink" />
-        <button
-          type="button"
-          onClick={ () => history.push('/meals') }
-          data-testid="meals-bottom-btn"
-        >
-          página de comidas
-        </button>
-      </div>
+    <div data-testid="footer" style={ { bottom: 0, position: 'fixed' } }>
 
-    </>
+      <input
+        type="image"
+        alt="drinks"
+        src={ drink }
+        onClick={ () => history.push('/drinks') }
+        data-testid="drinks-bottom-btn"
+      />
 
+      <input
+        type="image"
+        alt="meals"
+        src={ food }
+        onClick={ () => history.push('/meals') }
+        data-testid="meals-bottom-btn"
+      />
+
+    </div>
   );
 }
