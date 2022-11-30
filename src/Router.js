4cd3cea1a537d06a45f90056911d774162b59f03
CalebeLAR <ca6ele@gmail.com>
2022-11-30 16:53:34 -0400

    feat:req24 - RecipeDetais.jsx - agora a pagina faz um fetch especifico, dependendo se é o detalhe de uma comida ou uma bebida

--- src/Router.js
+++ src/Router.js
@@ -18,9 +18,9 @@ export default function Router() {
           path="/drinks/:id-da-receita/in-progress"
           component={ RecipesInProgress }
         />
-        {/* linha 24: so reconhece o ID se a rota for ecrita assim */}
+        {/* linha 22 e 23 : so reconhece o ID se a rota for ecrita assim */}
         <Route path="/meals/:idDaReceita" component={ RecipeDetails } />
-        <Route path="/drinks/:id-da-receita" component={ RecipeDetails } />
+        <Route path="/drinks/:idDaReceita" component={ RecipeDetails } />
         <Route path="/done-recipes" component={ DoneRecipes } />
         <Route path="/favorite-recipes" component={ FavoriteRecipes } />
         <Route path="/profile" component={ Profile } />
--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -2,9 +2,8 @@ import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 export default function RecipeDetails(props) {
-  const { match: { params: { idDaReceita } } } = props;
   const fetchMealDetails = (idMeal) => {
-    idMeal = '53065';
+    // idMeal = '53065';
     const URL = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
     fetch(URL)
       .then((response) => response.json())
@@ -12,22 +11,35 @@ export default function RecipeDetails(props) {
       .catch((error) => console.log(error));
     console.log('fetchMealDetails');
   };
+
   const fetchDrinkDetails = (idDrink) => {
-    idDrink = '15997';
+    // idDrink = '15997';
     const URL = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idDrink}`;
     fetch(URL)
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error));
     console.log('fetchDrinkDetails');
   };
+
+  const whereDidTheIdComeFrom = () => {
+    const { match: { params: { idDaReceita } } } = props;
+    const { history: { location: { pathname } } } = props;
+    if (pathname.includes('meals')) {
+      fetchMealDetails(idDaReceita);
+    }
+    if (pathname.includes('drinks')) {
+      fetchDrinkDetails(idDaReceita);
+    }
+  };
+
   useEffect(() => {
-    fetchMealDetails();
-    fetchDrinkDetails();
+    whereDidTheIdComeFrom();
   });
+
   return (
     <main>
       <h1>RecipeDetails</h1>
-      <p>{idDaReceita}</p>
     </main>
   );
 }
@@ -36,6 +48,11 @@ RecipeDetails.propTypes = {
   match: PropTypes.shape({
     params: PropTypes.shape({
       idDaReceita: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+  history: PropTypes.shape({
+    location: PropTypes.shape({
+      pathname: PropTypes.string.isRequired,
     }).isRequired,
   }).isRequired,
 };
