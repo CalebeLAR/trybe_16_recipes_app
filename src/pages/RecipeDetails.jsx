523382b2c2ecb8f5626f43b8fb05cf64c87db02e
CalebeLAR <ca6ele@gmail.com>
2022-11-30 16:11:12 -0400

    feat:req24 - RecipeDetais.jsx - implementando o fetch da receita em específico

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -3,10 +3,21 @@ import PropTypes from 'prop-types';
 
 export default function RecipeDetails(props) {
   const { match: { params: { idDaReceita } } } = props;
-  const fetchMealDetails = () => {
+  const fetchMealDetails = (idMeal) => {
+    idMeal = '53065';
+    const URL = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+    fetch(URL)
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error));
     console.log('fetchMealDetails');
   };
-  const fetchDrinkDetails = () => {
+  const fetchDrinkDetails = (idDrink) => {
+    idDrink = '15997';
+    const URL = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idDrink}`;
+    fetch(URL)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
     console.log('fetchDrinkDetails');
   };
   useEffect(() => {
