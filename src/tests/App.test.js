f17a1528d151e6ba8a2fcd1874788e420c541fe0
Matheus <matheuspaiva93@gmail.com>
2022-11-30 13:15:50 -0300

    requisitos 2-6 feitos

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,10 +1,21 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
 import App from '../App';
 
-test('Farewell, front-end', () => {
+test('Testa pagina de login', () => {
   // Este arquivo pode ser modificado ou deletado sem problemas
   render(<App />);
-  const linkElement = screen.getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
+  const emailInput = screen.getByTestId(/email-input/i);
+  expect(emailInput).toBeInTheDocument();
+  const passwordInput = screen.getByTestId(/password-input/i);
+  expect(passwordInput).toBeInTheDocument();
+  const buttonLogin = screen.getByTestId(/login-submit-btn/i);
+  expect(buttonLogin).toBeInTheDocument();
+  expect(buttonLogin).toBeDisabled();
+  userEvent.type(emailInput, 'teste@email.com');
+  userEvent.type(passwordInput, '1234567');
+  expect(buttonLogin).not.toBeDisabled();
+  userEvent.click(buttonLogin);
+  // expect(localStorage.getItem('user')).to.deep.eq({ email: 'teste@email.com' });
 });
