29ca5001df0c1be2cc2a53234b9576fa69573092
Diogo Martins <me.martinsfe@gmail.com>
2022-12-01 09:43:39 -0300

    feat: adiciona regra que dispara alerta na buscar
    Quando selecionado o radio First Letter, é disparado um alerta quando o usuário digitar mais de uum caractere no campo de busca, e então o input é limpo.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,33 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function SearchBar() {
+  const [searchRadioChecked, setSearchRadioChecked] = useState('ingredient');
+  const [search, setSearch] = useState('');
+
+  const handleSearch = ({ target }) => {
+    if (searchRadioChecked === 'firstLetter'
+    && search.length > 0) {
+      global.alert('Your search must have only 1 (one) character');
+      setSearch('');
+    } else {
+      setSearch(target.value);
+    }
+  };
+
   return (
     <div className="search">
       <input
         data-testid="search-input"
         type="search"
-        name=""
         placeholder="Search"
+        value={ search }
+        onChange={ handleSearch }
       />
 
       <div>
         <label htmlFor="ingredient">
-          <input type="radio" name="" id="ingredient" />
+          <input
+            data-testid="ingredient-search-radio"
+            type="radio"
+            name="search-radios"
+            id="ingredient"
+            value="ingredient"
+            onChange={ ({ target }) => { setSearchRadioChecked(target.value); } }
+            checked={ searchRadioChecked === 'ingredient' }
+          />
           Ingredient
         </label>
 
         <label htmlFor="name">
-          <input type="radio" name="" id="name" />
+          <input
+            data-testid="name-search-radio"
+            type="radio"
+            name="search-radios"
+            id="name"
+            value="name"
+            onChange={ ({ target }) => { setSearchRadioChecked(target.value); } }
+            checked={ searchRadioChecked === 'name' }
+          />
           Name
         </label>
 
         <label htmlFor="firstLetter">
-          <input type="radio" name="" id="firstLetter" />
+          <input
+            data-testid="first-letter-search-radio"
+            type="radio"
+            name="search-radios"
+            id="firstLetter"
+            value="firstLetter"
+            onChange={ ({ target }) => { setSearchRadioChecked(target.value); } }
+            checked={ searchRadioChecked === 'firstLetter' }
+          />
           First Letter
         </label>
       </div>
 
-      <button type="button">SEARCH</button>
+      <button
+        data-testid="exec-search-btn"
+        type="button"
+      >
+        SEARCH
+
+      </button>
     </div>
   );
 }
