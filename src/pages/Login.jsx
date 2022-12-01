ac6dffe224e16446dd663e6f7a340e078e00473a
CalebeLAR <ca6ele@gmail.com>
2022-12-01 14:45:22 -0400

    fix: Login.jsx tirando links

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import rockGlass from '../images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css';
@@ -15,8 +14,6 @@ export default function Login() {
       >
         LOGIN
       </object>
-      <Link to="/meals/53065"> COMIDA detalhes</Link>
-      <Link to="/drinks/15997"> BEBIDA detalhes</Link>
     </div>
   );
 }
