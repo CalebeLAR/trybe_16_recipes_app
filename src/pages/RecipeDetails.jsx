97d51cc7c0e3ffcd91a28c732a6d82fedcd81d18
CalebeLAR <ca6ele@gmail.com>
2022-12-01 14:20:51 -0400

    feat:req26 - RecipeDetails.jsx - fazendo o fatch pro endpoint de bebidas

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -5,49 +5,65 @@ import MealsCard from '../components/MealsCard';
 
 export default function RecipeDetails(props) {
   const { history: { location: { pathname } } } = props;
-  const [dataDetails, setDataDetails] = useState(false);
+  // const [dataDetails, setDataDetails] = useState(false);
+  const [dataRecommendations, setDataRecommendations] = useState(false);
 
-  const fetchMealDetails = (idMeal) => {
-    // idMeal = '53065';
-    const URL = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+  // const fetchMealDetails = (idMeal) => {
+  //   // idMeal = '53065';
+  //   const URL = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+  //   fetch(URL)
+  //     .then((response) => response.json())
+  //     .then((data) => setDataDetails(data.meals[0]))
+  //     .catch((error) => console.log(error));
+  // };
+  // const fetchDrinkDetails = (idDrink) => {
+  //   // idDrink = '15997';
+  //   const URL = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idDrink}`;
+  //   fetch(URL)
+  //     .then((response) => response.json())
+  //     .then((data) => setDataDetails(data.drinks[0]))
+  //     .catch((error) => console.log(error));
+  // };
+  // const fetchDetails = () => {
+  //   // Pega o id e o path da pagina parar renderizar apenas comidas ou bebidas da página;
+  //   const { match: { params: { idDaReceita } } } = props;
+  //   if (pathname.includes('meals')) {
+  //     fetchMealDetails(idDaReceita);
+  //   }
+  //   if (pathname.includes('drinks')) {
+  //     fetchDrinkDetails(idDaReceita);
+  //   }
+  // };
+
+  const fetchMealsRecommendations = () => {
+    // recomenda bebidas;
+    const URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
     fetch(URL)
       .then((response) => response.json())
-      .then((data) => setDataDetails(data.meals[0]))
+      .then((data) => setDataRecommendations(data))
       .catch((error) => console.log(error));
   };
 
-  const fetchDrinkDetails = (idDrink) => {
-    // idDrink = '15997';
-    const URL = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idDrink}`;
-    fetch(URL)
-      .then((response) => response.json())
-      .then((data) => setDataDetails(data.drinks[0]))
-      .catch((error) => console.log(error));
-  };
-
-  const whereDidTheIdComeFrom = () => {
-    const { match: { params: { idDaReceita } } } = props;
-    if (pathname.includes('meals')) {
-      fetchMealDetails(idDaReceita);
-    }
-    if (pathname.includes('drinks')) {
-      fetchDrinkDetails(idDaReceita);
-    }
-  };
   useEffect(() => {
-    whereDidTheIdComeFrom();
+    // fetchDetails();
+    fetchMealsRecommendations();
   }, []);
   return (
     <main>
       <h1>RecipeDetails</h1>
-      {
+      {/* {
         (Object.keys(dataDetails).length > 0 && pathname.includes('meals'))
           && <MealsCard mealDetails={ dataDetails } />
       }
       {
         (Object.keys(dataDetails).length > 0 && pathname.includes('drinks'))
           && <DrinksCard drinkDetails={ dataDetails } />
-      }
+      } */}
+      <h1>teste para recomendações</h1>
+      {/* {
+        (Object.keys(dataDetails).length > 0 && pathname.includes('drinks'))
+        && <RecommendationsCard RecommendationsCard={ RecommendationsCard } />
+      } */}
     </main>
   );
 }
