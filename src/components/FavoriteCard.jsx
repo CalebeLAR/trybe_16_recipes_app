ef4b35336cde9dcfe9990150ef685a7a9a5544ff
CalebeLAR <ca6ele@gmail.com>
2022-12-08 14:24:46 -0400

    feat:req50 - FavoriteCard.jsx - refatorando o código e passando e corrigindo um data test ID

--- src/components/FavoriteCard.jsx
+++ src/components/FavoriteCard.jsx
@@ -1,14 +1,5 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-// * Todos os data-testids estão presentes:
-//  O botão de filtro `All` deve ter o atributo `data-testid="filter-by-all-btn"`;
-//  O botão de filtro `Meals` deve ter o atributo `data-testid="filter-by-meal-btn"`;
-//  O botão de `Drinks` deve ter o atributo `data-testid="filter-by-drink-btn"`;
-//  A imagem do card de receita deve ter o atributo `data-testid="${index}-horizontal-image"`;
-//  O texto da categoria da receita deve ter o atributo `data-testid="${index}-horizontal-top-text"`;
-//  O texto do nome da receita deve ter o atributo `data-testid="${index}-horizontal-name"`;
-//  O elemento de compartilhar a receita deve ter o atributo `data-testid="${index}-horizontal-share-btn"`;
-//  O elemento de favoritar a receita deve ter o atributo `data-testid="${index}-horizontal-favorite-btn"`;/
 
 export default function FavoriteCard({ filteredRecipes }) {
   return (
@@ -16,86 +7,80 @@ export default function FavoriteCard({ filteredRecipes }) {
       {
         filteredRecipes.map((favRecipe, index) => {
           const { type, name, image, category, alcoholicOrNo, nationality } = favRecipe;
-          if (type === 'meal') {
-            return (
-              <div key={ name }>
-                <img
-                  data-testid={ `${index}-horizontal-image` }
-                  src={ image }
-                  alt={ name }
-                  width={ 200 }
-                />
-                <div>
-                  <p
-                    data-testid={ `${index}-horizontal-name` }
-                  >
-                    {name}
-                  </p>
-                  <p
-                    data-testid={ `${index}-horizontal-top-text` }
-                  >
-                    {`categoria: ${category}`}
-                  </p>
-                  <p>{`origem: ${nationality}`}</p>
-                </div>
-                <div>
-                  <button
-                    data-testid={ `${index}-horizontal-share-btn` }
-                    type="button"
-                  >
-                    compartilhar receita
-                  </button>
-                  <button
-                    data-testid={ `data-testid="${index}-horizontal-favorite-btn` }
-                    type="button"
-                  >
-                    Favorite
-                  </button>
-                </div>
+          return (type === 'meal') ? (
+            <div key={ name }>
+              <img
+                data-testid={ `${index}-horizontal-image` }
+                src={ image }
+                alt={ name }
+                width={ 200 }
+              />
+              <div>
+                <p
+                  data-testid={ `${index}-horizontal-name` }
+                >
+                  {name}
+                </p>
+                <p
+                  data-testid={ `${index}-horizontal-top-text` }
+                >
+                  {`categoria: ${category}`}
+                </p>
+                <p>{`origem: ${nationality}`}</p>
               </div>
-            );
-          }
-          if (type === 'drink') {
-            return (
-              <div key={ name }>
-                <img
-                  data-testid={ `${index}-horizontal-image` }
-                  src={ image }
-                  alt={ name }
-                  width={ 200 }
-                />
-                <div>
-                  <p
-                    data-testid={ `${index}-horizontal-name` }
-                  >
-                    {name}
-                  </p>
-                  <p
-                    data-testid={ `${index}-horizontal-top-text` }
-                  >
-                    {`categoria: ${category}`}
-                  </p>
-                  <p>{`origem: ${nationality}`}</p>
-                  <p>{`alcolica: ${alcoholicOrNo}`}</p>
-                </div>
-                <div>
-                  <button
-                    data-testid={ `${index}-horizontal-share-btn` }
-                    type="button"
-                  >
-                    compartilhar receita
-                  </button>
-                  <button
-                    data-testid={ `data-testid="${index}-horizontal-favorite-btn` }
-                    type="button"
-                  >
-                    Favorite
-                  </button>
-                </div>
+              <div>
+                <button
+                  data-testid={ `${index}-horizontal-share-btn` }
+                  type="button"
+                >
+                  compartilhar receita
+                </button>
+                <button
+                  data-testid={ `${index}-horizontal-favorite-btn` }
+                  type="button"
+                >
+                  Favorite
+                </button>
               </div>
-            );
-          }
-          return null;
+            </div>
+          ) : (
+            <div key={ name }>
+              <img
+                data-testid={ `${index}-horizontal-image` }
+                src={ image }
+                alt={ name }
+                width={ 200 }
+              />
+              <div>
+                <p
+                  data-testid={ `${index}-horizontal-name` }
+                >
+                  {name}
+                </p>
+                <p
+                  data-testid={ `${index}-horizontal-top-text` }
+                >
+                  {`categoria: ${category}`}
+                </p>
+                <p>{`origem: ${nationality}`}</p>
+                <p>{`alcolica: ${alcoholicOrNo}`}</p>
+              </div>
+              <div>
+                <button
+                  data-testid={ `${index}-horizontal-share-btn` }
+                  type="button"
+                >
+                  compartilhar receita
+                </button>
+                <button
+                  data-testid={ `${index}-horizontal-favorite-btn` }
+                  type="button"
+                >
+                  Favorite
+                </button>
+              </div>
+            </div>
+          );
         })
       }
     </section>
--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -13,31 +13,35 @@ import Header from '../components/Header';
 // * O elemento de favoritar a receita deve ter o atributo `data-testid="${index}-horizontal-favorite-btn"`;/
 
 // testes
-const testRecipe = [{
-  id: '13501',
-  type: 'drink',
-  nationality: 'nacionalidade-da-receita-ou-texto-vazio',
-  category: 'categoria-da-receita-ou-texto-vazio',
-  alcoholicOrNot: 'alcoholic-ou-non-alcoholic-ou-texto-vazio',
-  name: 'ABC',
-  image: 'https://www.thecocktaildb.com/images/media/drink/tqpvqp1472668328.jpg',
-},
-{
-  id: '52977',
-  type: 'meal',
-  nationality: 'nacionalidade-da-receita-ou-texto-vazio',
-  category: 'categoria-da-receita-ou-texto-vazio',
-  alcoholicOrNot: 'alcoholic-ou-non-alcoholic-ou-texto-vazio',
-  name: 'Corba',
-  image: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg',
-}];
+// const testRecipe = [{
+//   id: '13501',
+//   type: 'drink',
+//   nationality: 'nacionalidade-da-receita-ou-texto-vazio',
+//   category: 'categoria-da-receita-ou-texto-vazio',
+//   alcoholicOrNot: 'alcoholic-ou-non-alcoholic-ou-texto-vazio',
+//   name: 'ABC',
+//   image: 'https://www.thecocktaildb.com/images/media/drink/tqpvqp1472668328.jpg',
+// },
+// {
+//   id: '52977',
+//   type: 'meal',
+//   nationality: 'nacionalidade-da-receita-ou-texto-vazio',
+//   category: 'categoria-da-receita-ou-texto-vazio',
+//   alcoholicOrNot: 'alcoholic-ou-non-alcoholic-ou-texto-vazio',
+//   name: 'Corba',
+//   image: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg',
+// }];
 
-localStorage.setItem('favoriteRecipes', JSON.stringify(testRecipe));
+// localStorage.setItem('favoriteRecipes', JSON.stringify(testRecipe));
 
-const storageFavorites = localStorage.getItem('favoriteRecipes');
-const favoriteRecipes = JSON.parse(storageFavorites);
+const storageFavorites = (localStorage.getItem('favoriteRecipes'));
+// const storageDoneRecipes = (localStorage.getItem('doneRecipes'));
+const favoriteRecipes = JSON.parse(storageFavorites) || [];
+// const doneRecipes = JSON.parse(storageDoneRecipes) || [];
+const allRecipes = [...favoriteRecipes];
+
 export default function FavoriteRecipes() {
-  const [filteredRecipes, setFilteredRecipes] = useState(favoriteRecipes);
+  const [filteredRecipes] = useState(allRecipes);
 
   return (
     <main>
