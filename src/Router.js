dba8d0a06b8e25d023a48387f10f8d5d40cfcbd1
Diogo Martins <me.martinsfe@gmail.com>
2022-12-13 01:20:02 -0300

    feat: adiciona testes

--- src/Router.js
+++ src/Router.js
@@ -11,28 +11,28 @@ import RecipesInProgress from './pages/RecipesInProgress';
 
 export default function Router() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route
-          path="/meals/:idDaReceita/in-progress"
-          component={ RecipesInProgress }
-        />
-        <Route
-          path="/drinks/:idDaReceita/in-progress"
-          component={ RecipesInProgress }
-        />
+    // <BrowserRouter>
+    <Switch>
+      <Route
+        path="/meals/:idDaReceita/in-progress"
+        component={ RecipesInProgress }
+      />
+      <Route
+        path="/drinks/:idDaReceita/in-progress"
+        component={ RecipesInProgress }
+      />
 
-        {/* linha 22 e 23 : so reconhece o ID se a rota for escrita assim */}
-        <Route path="/meals/:idDaReceita" component={ RecipeDetails } />
-        <Route path="/drinks/:idDaReceita" component={ RecipeDetails } />
-        <Route path="/done-recipes" component={ DoneRecipes } />
-        <Route path="/favorite-recipes" component={ FavoriteRecipes } />
-        <Route path="/profile" component={ Profile } />
-        <Route path="/meals" component={ Recipes } />
-        <Route path="/drinks" component={ Recipes } />
-        <Route exact path="/" component={ Login } />
-        <Route path="*" component={ NotFound } />
-      </Switch>
-    </BrowserRouter>
+      {/* linha 22 e 23 : so reconhece o ID se a rota for escrita assim */}
+      <Route path="/meals/:idDaReceita" component={ RecipeDetails } />
+      <Route path="/drinks/:idDaReceita" component={ RecipeDetails } />
+      <Route path="/done-recipes" component={ DoneRecipes } />
+      <Route path="/favorite-recipes" component={ FavoriteRecipes } />
+      <Route path="/profile" component={ Profile } />
+      <Route path="/meals" component={ Recipes } />
+      <Route path="/drinks" component={ Recipes } />
+      <Route exact path="/" component={ Login } />
+      <Route path="*" component={ NotFound } />
+    </Switch>
+    // </BrowserRouter>
   );
 }
